639228f30c1fe228826f19ddc72662e1d3c05dfb
Corentin <corentinlg.dev@gmail.com>
2026-03-05 09:19:41 +0100

    feat: add prisma studio

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ echo "Starting package.json and schema.prisma watchers..."
 npx nodemon --legacy-watch --watch package.json --ext json --delay 500ms --exec "npm install && $@" &
 npx nodemon --legacy-watch --watch prisma/schema.prisma --ext prisma --delay 500ms --exec "npx prisma db push" &
 
+npx prisma studio
+
 echo "Starting the app..."
 
 wait
